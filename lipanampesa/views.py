0223299969d59905b0a00358e319c4bdf26ce74a
Koech-code <nixonkipkorir01@gmail.com>
2023-02-25 16:17:43 +0300

    make changes to views

--- lipanampesa/views.py
+++ lipanampesa/views.py
@@ -6,5 +6,4 @@ from .c2b import lipa_na_mpesa
 @api_view(['POST'])
 def lipa_na_mpesa_online(request):
     mpesa_payment = lipa_na_mpesa()
-    print(mpesa_payment)
     return Response({"mpesa_payment": mpesa_payment})
